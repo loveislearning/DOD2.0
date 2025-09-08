10e1ea07dfb0a88920195da60592096b43fca330
Mew <shallychauhan127@gmail.com>
2025-09-08 09:29:14 +0530

    .

--- login.js
+++ login.js
@@ -1,4 +1,3 @@
-// Toggle forms
 document.getElementById("show-signup").addEventListener("click", () => {
   document.getElementById("login-box").classList.add("hidden");
   document.getElementById("signup-box").classList.remove("hidden");
@@ -9,16 +8,15 @@ document.getElementById("show-login").addEventListener("click", () => {
   document.getElementById("login-box").classList.remove("hidden");
 });
 
-// SIGN UP
 document.getElementById("signup-form").addEventListener("submit", (e) => {
   e.preventDefault();
   alert("✅ Account created successfully!");
-  window.location.href = "index.html"; // Redirect to index.html
+  window.location.href = "index.html"; 
 });
 
 // LOGIN
 document.getElementById("login-form").addEventListener("submit", (e) => {
   e.preventDefault();
   alert("✅ Login successful!");
-  window.location.href = "index.html"; // Redirect to index.html
+  window.location.href = "index.html"; 
 });
